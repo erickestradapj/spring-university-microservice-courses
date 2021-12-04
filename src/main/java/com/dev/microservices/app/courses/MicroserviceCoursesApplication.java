ffe25169df5633b66dce4478b3a7523e79b12ccd
Erick Estrada <erickestradapj@gmail.com>
2021-12-04 16:54:48 -0500

    starting project

---
+++ src/main/java/com/dev/microservices/app/courses/MicroserviceCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.dev.microservices.app.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCoursesApplication.class, args);
+	}
+
+}
